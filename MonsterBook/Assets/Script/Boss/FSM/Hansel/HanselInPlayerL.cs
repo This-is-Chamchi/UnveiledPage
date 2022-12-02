b3e7c944c5146a638e4704df325fabb3f49365b2
HYEON2-400\hyeon-2-400 <ymlee1633@gmail.com>
2022-12-02 16:35:58 +0900

    배치기 밀리는 힘 수정

--- MonsterBook/Assets/Script/Boss/FSM/Hansel/HanselInPlayerL.cs
+++ MonsterBook/Assets/Script/Boss/FSM/Hansel/HanselInPlayerL.cs
@@ -7,10 +7,12 @@ public class HanselInPlayerL : MonoBehaviour
     // Start is called before the first frame update
 
     private GameObject Player;
-
+    private GameObject Hansel;
+    private int dir;
     void Start()
     {
         Player = GameObject.FindWithTag("Player");
+        Hansel = GameObject.FindWithTag("Boss");
     }
 
     // Update is called once per frame
@@ -21,17 +23,36 @@ public class HanselInPlayerL : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if(Player.transform.position.x > Hansel.transform.position.x)
+        {
+            dir = 1;
+        }
+        else
+        {
+            dir = -1;
+        }
+
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            other.transform.position = new Vector3(other.transform.position.x + 0.08f, other.transform.position.y, other.transform.position.z);
+            other.transform.position = new Vector3(other.transform.position.x + 0.08f * dir, other.transform.position.y, other.transform.position.z);
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
+
+        if (Player.transform.position.x > Hansel.transform.position.x)
+        {
+            dir = 1;
+        }
+        else
+        {
+            dir = -1;
+        }
+
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            other.transform.position = new Vector3(other.transform.position.x + 0.08f, other.transform.position.y, other.transform.position.z);
+            other.transform.position = new Vector3(other.transform.position.x + 0.08f * dir, other.transform.position.y, other.transform.position.z);
         }
     }
 
